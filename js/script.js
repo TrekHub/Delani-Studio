a779aad8a31fe73c87d37ee2193180fc6f0e5c92
TrekHub <teddyomondi001@gmail.com>
2021-11-21 13:07:16 +0300

    add the what we do section

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$('.what1').click(function () {
+    $('#what-details1').toggle()
+    $('.what1').hide()
+
+})
+
+$('.what2').click(function () {
+    $('#what-details2').show()
+    $('.what2').hide()
+
+})
+
+$('.what3').click(function () {
+    $('#what-details3').show()
+    $('.what3').hide()
+
+})
