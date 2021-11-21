d6ffaa517bf9361354f70baf7ac95411718be7fd
TrekHub <teddyomondi001@gmail.com>
2021-11-21 22:29:02 +0300

    fix the toggle issue

--- js/script.js
+++ js/script.js
@@ -1,17 +1,32 @@
 $('.what1').click(function () {
-    $('#what-details1').toggle()
+    $('#what-details1').show()
+    $('#what-details1').click(function() {
+        $('#what-details1').hide()
+        $('.what1').show()
+
+    })
     $('.what1').hide()
 
 })
 
 $('.what2').click(function () {
     $('#what-details2').show()
+    $('#what-details2').click(function() {
+        $('#what-details2').hide()
+        $('.what2').show()
+
+    })
     $('.what2').hide()
 
 })
 
 $('.what3').click(function () {
     $('#what-details3').show()
+    $('#what-details3').click(function() {
+        $('#what-details3').hide()
+        $('.what3').show()
+
+    })
     $('.what3').hide()
 
 })
